85cfb8a066fd40e2270d0c7b60349015553c40a1
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2025-01-31 21:04:49 +1300

    created menucontext, added one instance of data object

---
+++ Menu/Data/MenuContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Menu.Models;
+
+namespace Menu.Data
+{
+	public class MenuContext : DbContext
+	{
+		public MenuContext( DbContextOptions<MenuContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<DishIngredient>().HasKey(di => new { di.DishId, di.IngredientId });
+
+			modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+			modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+			modelBuilder.Entity<Dish>().HasData(
+				new Dish { Id=1, Name="Margerita", Price=6.99, ImageUrl= "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067" }
+			);
+			modelBuilder.Entity<Ingredient>().HasData(
+				new Ingredient { Id = 1, Name = "Tomato Sauce" },
+				new Ingredient { Id = 2, Name = "Mozzarella" },
+				new Ingredient { Id = 3, Name = "Basil" }
+			);
+
+			modelBuilder.Entity<DishIngredient>().HasData(
+				new DishIngredient { DishId = 1, IngredientId = 1 },
+				new DishIngredient { DishId = 1, IngredientId = 2 },
+				new DishIngredient { DishId = 1, IngredientId = 3 }
+			);
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		public DbSet<Dish> Dishes { get; set; }
+
+		public DbSet<Ingredient> Ingredients { get; set; }
+
+		public DbSet<DishIngredient> DishIngredients { get; set; }
+	}
+}
--- Menu/Models/Ingredient.cs
+++ Menu/Models/Ingredient.cs
@@ -1,10 +1,10 @@
 ﻿namespace Menu.Models
 {
-	public class Menu
+	public class Ingredient
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string ImageUrl { get; set; }
-		public double Price { get; set; }
+
+		public List<DishIngredient> DishIngredients { get; set; }
 	}
 }
