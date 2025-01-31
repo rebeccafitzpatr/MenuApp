974f0fc9cd19e3df301d3c4749d8f987c83c3a74
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2025-01-31 19:40:35 +1300

    Add project files.

---
+++ Menu/Models/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace Menu.Models
+{
+	public class Menu
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string ImageUrl { get; set; }
+		public double Price { get; set; }
+	}
+}
